d203a4fd4be89c51ba513ef4239de2553b6ebb62
Diego Contreras <diegocontreraskwk14@gmail.com>
2026-08-25 11:20:57 -0400

    Reemplaza ejercicio pulsador/LED por RGB con 3 potenciómetros
    Controla un led RGB (pines 6, 10, 11) según lectura de 3 potenciómetros
    en A0-A2, mapeando 0-1023 a 0-255. Actualiza diagrama de conexión.

--- src/uno/main.cpp
+++ src/uno/main.cpp
@@ -1,14 +1,25 @@
 #include <Arduino.h>
 
-const int BUTTON_PIN = 2;
-const int LED_PIN = 11;
+const int POT_R_PIN = A0;
+const int POT_G_PIN = A1;
+const int POT_B_PIN = A2;
+
+const int LED_R_PIN = 6;
+const int LED_G_PIN = 10;
+const int LED_B_PIN = 11;
 
 void setup() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_R_PIN, OUTPUT);
+  pinMode(LED_G_PIN, OUTPUT);
+  pinMode(LED_B_PIN, OUTPUT);
 }
 
 void loop() {
-  bool pressed = digitalRead(BUTTON_PIN) == LOW;
-  digitalWrite(LED_PIN, pressed ? HIGH : LOW);
+  int red = analogRead(POT_R_PIN) / 4;
+  int green = analogRead(POT_G_PIN) / 4;
+  int blue = analogRead(POT_B_PIN) / 4;
+
+  analogWrite(LED_R_PIN, red);
+  analogWrite(LED_G_PIN, green);
+  analogWrite(LED_B_PIN, blue);
 }
